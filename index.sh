50aed648fab25d438c90029ec6a94cdabd09a56b
なおしむ <nao_pillows@yahoo.co.jp>
2023-03-05 07:30:46 +0000

    無駄もあるが一旦コミット

--- index.sh
+++ index.sh
@@ -4,4 +4,4 @@ curl https://note.com/naosim/rss > ./xml/blog.xml
 curl https://www.youtube.com/feeds/videos.xml?channel_id=UCDGQKL08u5p9R8HBIYR9ARw > ./xml/youtube_pari.xml
 curl https://www.youtube.com/feeds/videos.xml?channel_id=UCogqz-OIoF3rQ2iFhWbP0FA > ./xml/youtube.xml
 
-node index.js
+# node index.js
--- parse.mjs
+++ parse.mjs
@@ -1,3 +1,22 @@
 export function parse(xml) {
-    return []
+  if(xml.indexOf('<item>') != -1 && xml.indexOf('<entry>') != -1) {
+    throw new Error('itemとentryの両方ある');
+  } else if(xml.indexOf('<item>') != -1) {
+    return parseWithItem(xml);
+  } else if(xml.indexOf('<entry>') != -1) {
+    return parseWithEntry(xml);
+  } else {
+    throw new Error('itemもentryもない');
+  }
+}
+
+
+export function parseWithItem(xml) {
+  return []
+
+}
+
+export function parseWithEntry(xml) {
+  return []
+
 }
