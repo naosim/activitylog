6d5306a45c0d0823ce7eac29f1ea2712847134ed
naosim <naosms-other@yahoo.co.jp>
2024-07-07 18:01:48 +0900

    整理

--- src/activityLogs.mjs
+++
@@ -1,9 +0,0 @@
-import { blogLogs } from "./blogLog.mjs";
-import { youtubeLogs } from "./youtubeLogs.mjs";
-import { otherLogs } from "./otherlog.mjs";
-
-export const activityLogs = [...blogLogs, ...youtubeLogs, ...otherLogs].sort(
-  (a, b) => a.date.getTime() - b.date.getTime()
-);
-
-// console.log(activityLogs);
